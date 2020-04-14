7bdd6ad018d5f47886e24684170397ceff3ea20c
olafzi <57198166+olafzi@users.noreply.github.com>
2020-04-13 20:11:01 -0700

    Backend implementation. Query a word from OwlBot, display the Word, Definition and an Example. Check if connection is active upon initialization, and then every time a new entry is entered. If entry is null or blank, display alert and disregard entry to prevent parsing issues.

---
+++ CS431_HW6/Dictionary.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace CS431_HW6
+{
+    public partial class Dictionary
+    {
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("definition")]
+        public string Definition { get; set; }
+
+        [JsonProperty("example")]
+        public string Example { get; set; }
+    }
+}
--- CS431_HW6/MainPage.xaml.cs
+++ CS431_HW6/MainPage.xaml.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace CS431_HW6
@@ -16,6 +19,57 @@ namespace CS431_HW6
         public MainPage()
         {
             InitializeComponent();
+            CheckConnection();
+        }
+
+        public bool CheckConnection()
+        {
+            var current = Connectivity.NetworkAccess;
+
+            if (current == NetworkAccess.Internet)
+            {
+                // Connection to internet is available
+                return true;
+            }
+
+
+            DisplayAlert("Lonely local sheep", "If you have no internet, how will you see results, hm?", "OK");
+            return false;
+        }
+
+        private void Handle_QueryWord(object sender, EventArgs e)
+        {
+            string WordQuery = WordBoxInput.Text;
+            if (string.IsNullOrWhiteSpace(WordQuery))
+            { 
+                DisplayAlert("404: Word not found", "A dictionary might have an empty page, but fill this search box, please", "OK");
+            }
+            else
+
+            if (CheckConnection())
+            {
+                WordQuery = WordQuery.ToLower();
+                GetWordInfo(WordQuery);
+            }
+        }
+
+        async void GetWordInfo(string WordQuery)
+        {
+            var client = new HttpClient();
+            var OwlBotAddress = "https://owlbot.info/api/v2/dictionary/" + WordQuery;
+            var uri = new Uri(OwlBotAddress);
+
+            List<Dictionary> wordData = new List<Dictionary>();
+            var response = await client.GetAsync(uri);
+
+            if (response.IsSuccessStatusCode)
+            {
+                
+                var jsonContent = await response.Content.ReadAsStringAsync();
+                wordData = JsonConvert.DeserializeObject<List<Dictionary>>(jsonContent);
+            }
+
+            WordDataView.ItemsSource = wordData;
         }
     }
 }
